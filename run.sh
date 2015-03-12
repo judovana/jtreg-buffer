59f737ab4a160bacbb83dbcb3964dce54bacee66
Jiri Vanek <jvanek@redhat.com>
2015-03-12 15:55:20 +0100

    added possibility to run only one bug set of reproducers

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 JAVA=$1
 TIME=`date +%s`
+BUGID=$2
+
+if [ "x$BUGID" != "x" ] ; then 
+  BUGID="-bug:$BUGID"
+fi;
 
 echo Running with $JAVA...
 
@@ -8,5 +13,6 @@ mkdir -p test.${TIME}/jdk/JTwork test.${TIME}/jdk/JTreport
 java -jar jtreg.jar -v1 -a -ignore:quiet \
 		-w:test.${TIME}/jdk/JTwork -r:test.${TIME}/jdk/JTreport \
 		-jdk:$JAVA \
+		$BUGID \
 		test \
 	    | tee test.${TIME}/tests.log
