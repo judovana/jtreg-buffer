d9b4747c4d0fe42dd1a82895716f11098a9cdff1
Jiri Vanek <jvanek@redhat.com>
2015-03-13 14:15:45 +0100

    Added 6804998

---
+++ test/internal/6804998/Test.java
@@ -0,0 +1,76 @@
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.image.ImageObserver;
+
+/* @test
+   @bug 6804998
+   @summary  invalid gif read issue
+   @run shell Test.sh
+*/
+public class Test {
+
+    private static final boolean verbose = Boolean.getBoolean("verbose");
+
+    public static void main(String[] args) {
+        String fname = "test.gif";
+
+        if (args.length > 0) {
+            fname = args[0];
+        }
+
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Image img = tk.createImage(fname);
+
+        Observer o = new Observer();
+        tk.prepareImage(img, -1, -1, o);
+        System.out.println("Test passed.");
+    }
+
+    private static class Observer implements ImageObserver {
+        public boolean imageUpdate(Image img, int infoflags, int x, int y,
+                                    int width, int height)
+        {
+            if (verbose) {
+                System.out.printf("Got update: %s, x=%d, y=%d, w=%d, h=%d\n",
+                                  dump(infoflags), x, y, width, height);
+            }
+            boolean isReady = ((infoflags & FRAMEBITS) != 0) ||
+                    ((infoflags & ALLBITS) != 0);
+
+            return !isReady;
+        }
+
+        private static String dump(int f) {
+            String res = "";
+            if ((f & ABORT) != 0) {
+                res += " ABORT";
+            }
+            if ((f & ALLBITS) != 0) {
+                res += " ALLBITS";
+            }
+            if ((f & ERROR) != 0) {
+                res += " ERROR";
+            }
+            if ((f & FRAMEBITS) != 0) {
+                res += " FRAMEBITS";
+            }
+            if ((f & HEIGHT) != 0) {
+                res += " HEIGHT";
+            }
+            if ((f & WIDTH) != 0) {
+                res += " WIDTH";
+            }
+            if ((f & PROPERTIES) != 0) {
+                res += " PROPERTIES";
+            }
+            if ((f & SOMEBITS) != 0) {
+                res += " SOMEBITS";
+            }
+            
+            return res;
+        }
+
+    }
+
+}
---
+++ test/internal/6804998/Test.sh
@@ -0,0 +1,35 @@
+echo $PWD
+
+FS="/"
+JAVAC=${TESTJAVA}${FS}bin${FS}javac
+JAVA=${TESTJAVA}${FS}bin${FS}java
+
+$JAVAC -d . $TESTSRC/Test.java 
+R=$?
+if [[ $R -ne 0 ]] ; then
+  echo "Compilation failed"
+  exit $R
+fi
+cp -v $TESTSRC/test.gif . 
+R=$?
+if [[ $R -ne 0 ]] ; then
+  echo "Copy of resource failed"
+  exit $R
+fi
+
+$JAVA  Test 
+R=$?
+if [[ $R -ne 0 ]] ; then
+  echo "run  failed"
+  exit $R
+fi
+
+
+ls test.gif
+R=$?
+if [[ $R -ne 0 ]] ; then
+  echo "no image here"
+  exit $R
+fi
+
+exit 0
