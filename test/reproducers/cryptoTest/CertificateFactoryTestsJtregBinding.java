ca6eba6088a9c1115fc66972ef99132f03f98eea
Zdenek Zambersky <zzambers@redhat.com>
2017-06-07 19:11:25 +0200

    cryptoTest: added CertificateFactoryTests

--- test/reproducers/cryptoTest/CertificateFactoryTestsJtregBinding.java
+++ test/reproducers/cryptoTest/CertificateFactoryTestsJtregBinding.java
@@ -1,11 +1,10 @@
-/**
- * WARNING build path must be regenerated with each test added:(
- * 
+/*
  * @test
  * @bug 6666666
  * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyGeneratorTests cryptotest.tests.SignatureTests cryptotest.tests.MessageDigestTests  cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.TransformServiceTests cryptotest.tests.CertificateFactoryTests
- * @run main/othervm cryptotest.tests.TestServices
+ * @run main/othervm cryptotest.tests.CertificateFactoryTests
  */
-public class TestServicesJtregBinding {
 
-}
+public class CipherTestsJtregBinding {
+
+    }
--- test/reproducers/cryptoTest/TestProvidersJtregBinding.java
+++ test/reproducers/cryptoTest/TestProvidersJtregBinding.java
@@ -1,7 +1,7 @@
 /*
  * @test
  * @bug 6666666
- * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyGeneratorTests cryptotest.tests.SignatureTests cryptotest.tests.MessageDigestTests  cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.TransformServiceTests
+ * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyGeneratorTests cryptotest.tests.SignatureTests cryptotest.tests.MessageDigestTests  cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.TransformServiceTests cryptotest.tests.CertificateFactoryTests
  * @run main/othervm cryptotest.tests.TestProviders
  */
 public class TestProvidersJtregBinding {
