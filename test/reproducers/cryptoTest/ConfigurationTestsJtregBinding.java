826d2c599f4f3c77083db88f24fffbf9bdb47aa3
mvala <mvala@redhat.com>
2017-06-29 17:33:02 +0200

    cryptotest - jtreg bindings for CertPathBuilder, Configuration, KeyFactory, KeyInfoFactory, KeyManagerFactory, KeyPairGenerator, Policy, SecretKeyFactory, SSLContext and TrustManagerFactory tests

---
+++ test/reproducers/cryptoTest/ConfigurationTestsJtregBinding.java
@@ -0,0 +1,10 @@
+/*
+ * @test
+ * @bug 6666666
+ * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyGeneratorTests cryptotest.tests.SignatureTests cryptotest.tests.MessageDigestTests  cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.TransformServiceTests cryptotest.tests.ConfigurationTests
+ * @run main/othervm cryptotest.tests.ConfigurationTests
+ */
+
+public class ConfigurationTestsJtregBinding {
+
+}
