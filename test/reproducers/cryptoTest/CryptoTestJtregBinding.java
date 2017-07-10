c6cbdf5675699976f4d24c619bcbba5c18416bd1
Jiri Vanek <jvanek@redhat.com>
2017-07-10 10:16:52 +0200

    Added all current tests to main test classapth

--- test/reproducers/cryptoTest/CryptoTestJtregBinding.java
+++ test/reproducers/cryptoTest/CryptoTestJtregBinding.java
@@ -1,9 +1,15 @@
+// generated as
+// a=`find cryptotest/ | grep .java | sed s/.java// | sed "s;/;.;g"`
+// echo $a
 /*
  * @test
  * @bug 6666666
- * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyGeneratorTests cryptotest.tests.SignatureTests cryptotest.tests.MessageDigestTests  cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.TransformServiceTests cryptotest.tests.CertificateFactoryTests
+ * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.KeyAgreementTests cryptotest.tests.KeyPairGeneratorTests cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyInfoFactoryTests cryptotest.tests.CertPathBuilderTests cryptotest.tests.AlgorithmParametersTests cryptotest.tests.AlgorithmParameterGeneratorTests cryptotest.tests.SSLContextTests cryptotest.tests.SecretKeyFactoryTests cryptotest.tests.CertPathValidatorTests cryptotest.tests.PolicyTests cryptotest.tests.SaslClientFactoryTests cryptotest.tests.KeyGeneratorTests cryptotest.tests.CertificateFactoryTests cryptotest.tests.ConfigurationTests cryptotest.tests.SignatureTests cryptotest.tests.KeyFactoryTests cryptotest.tests.TrustManagerFactoryTests cryptotest.tests.MessageDigestTests cryptotest.tests.TestProviders cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.GssApiMechanismTests cryptotest.tests.TerminalFactoryTests cryptotest.tests.TransformServiceTests cryptotest.tests.KeyManagerFactoryTests
+
  * @run main/othervm cryptotest.CryptoTest
  */
 public class CryptoTestJtregBinding {
 
 }
+
+
