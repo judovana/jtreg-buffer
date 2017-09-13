467a78a196a4ff01b06e7cc66ff076b0ead702db
Jiri Vanek <jvanek@redhat.com>
2017-09-13 15:16:13 +0200

    TransformServiceTests made generally working Initing all algorithms, but skipping usage of algorithms which seems to be broken in jdk, , but are beyond repair. However, we could just be wrong. by  <oklinovs@redhat.com>

--- test/reproducers/cryptoTest/cryptotest/tests/TransformServiceTests.java
+++ test/reproducers/cryptoTest/cryptotest/tests/TransformServiceTests.java
@@ -40,6 +40,7 @@ import cryptotest.utils.AlgorithmRunException;
 import cryptotest.utils.AlgorithmTest;
 import cryptotest.utils.TestResult;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
@@ -48,8 +49,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import javax.xml.crypto.Data;
+import javax.xml.crypto.MarshalException;
 import javax.xml.crypto.OctetStreamData;
 import javax.xml.crypto.dom.DOMStructure;
+import javax.xml.crypto.dsig.TransformException;
 import javax.xml.crypto.dsig.TransformService;
 import javax.xml.crypto.dsig.spec.TransformParameterSpec;
 import javax.xml.crypto.dsig.spec.XPathFilter2ParameterSpec;
@@ -58,7 +61,9 @@ import javax.xml.crypto.dsig.spec.XPathType;
 import javax.xml.crypto.dsig.spec.XSLTTransformParameterSpec;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /*
  * IwishThisCouldBeAtTest
@@ -74,79 +79,122 @@ public class TransformServiceTests extends AlgorithmTest {
         System.out.println(r.toString());
         r.assertItself();
     }
-
+ 
     @Override
     protected void checkAlgorithm(Provider.Service service, String alias) throws
             AlgorithmInstantiationException, AlgorithmRunException {
         try {
+            boolean shouldMarshal = true;
+            // By default we consider all algorithms as not working. See methods isKindOfWorking ans isTroubleMaker, which find those algorithms that are working.
+            boolean isTroublemaker = true;
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             documentBuilderFactory.setNamespaceAware(true);
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            String xslt
-                    = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                    + "<xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">"
-                    + "   <xsl:template match=\"/\">"
-                    + "      <html>"
-                    + "         <body>"
-                    + "            <p><xsl:value-of select=\"root/child/name\"/></p>"
-                    + "            <p><xsl:value-of select=\"root/child/age\"/></p>"
-                    + "         </body>"
-                    + "      </html>"
-                    + "   </xsl:template>"
-                    + "</xsl:stylesheet>";
-            InputStream xsltStream = new ByteArrayInputStream(xslt.getBytes(StandardCharsets.UTF_8));
-            Document document = documentBuilder.parse(xsltStream);
 
+            String xslt
+                    = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                    + "<xsl:stylesheet version=\"2.0\" type=\"text/xsl\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">\n"
+                    + "   <xsl:template match=\"/\">\n"
+                    + "      <html>\n"
+                    + "         <body>\n"
+                    + "            <p><xsl:value-of select=\"root/child/name\"/></p>\n"
+                    + "            <p><xsl:value-of select=\"root/child/age\"/></p>\n"
+                    + "         </body>\n"
+                    + "      </html>\n"
+                    + "   </xsl:template>\n"
+                    + "</xsl:stylesheet>\n";
+
+            String xml
+                    = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                    + "<root>\n"
+                    + "   <child>\n"
+                    + "<!-- comment -->\n"
+                    + "      <name>name1</name>\n"
+                    + "      <age>23</age>\n"
+                    + "   </child>\n"
+                    + "\n"
+                    + "   <child>\n"
+                    + "      <name>name2</name>\n"
+                    + "      <age>25</age>\n"
+                    + "   </child>\n"
+                    + "<!-- comment -->\n"
+                    + "</root>\n";
+
+            InputStream xmlStream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
+            Data data = new OctetStreamData(xmlStream);
+            DOMStructure xmlStructure;
+
+            InputStream xsltStream = new ByteArrayInputStream(xslt.getBytes(StandardCharsets.UTF_8));
+            Document xsltDocument = documentBuilder.parse(xsltStream);
+            DOMStructure stylesheet = new DOMStructure(xsltDocument.getDocumentElement());
             TransformService ts = TransformService.getInstance(alias, "DOM", service.getProvider());
             final TransformParameterSpec params;
             if (service.getAlgorithm().endsWith("/REC-xslt-19991116")) {
-                //Node element = Xml.fakeXml();
-                DOMStructure stylesheet = new DOMStructure(document.getDocumentElement());
                 XSLTTransformParameterSpec spec = new XSLTTransformParameterSpec(stylesheet);
                 params = spec;
             } else if (service.getAlgorithm().endsWith("/xmldsig-filter2")) {
                 List<XPathType> list = new ArrayList<>();
                 list.add(new XPathType("/", XPathType.Filter.UNION));
                 params = new XPathFilter2ParameterSpec(list);
-                return;
             } else if (service.getAlgorithm().endsWith("/REC-xpath-19991116")) {
                 params = new XPathFilterParameterSpec("/");
-                return;
+            } else if (isKindOfWorkingAlgorithm(service.getAlgorithm())) {
+                params = null;
+                shouldMarshal = false;
+                isTroublemaker = isTroublemaker(alias);
             } else {
                 params = null;
-                return;
             }
-            ts.init(params);
 
-            String xml
-                    = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                    + "<root>"
-                    + "   <child>"
-                    + "      <name>name1</name>"
-                    + "      <age>23</age>"
-                    + "   </child>"
-                    + "   <child>"
-                    + "      <name>name2</name>"
-                    + "      <age>25</age>"
-                    + "   </child>"
-                    + "</root>";
+            if (isTroublemaker) {
+                printResult("Troublemaking Transform Service, skipping usage part.");
+            } else {
+                ts.init(params);
+                if (shouldMarshal) {
 
-            InputStream stream = new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
-            Data data = new OctetStreamData(stream);
-            DOMStructure s = new DOMStructure(document.getDocumentElement());
+                    Document xmlDocument = documentBuilder.parse(xmlStream);
+                    xmlStructure = new DOMStructure(xmlDocument.getDocumentElement());
+                    ts.marshalParams(xmlStructure, null);
+                }
 
-            ts.marshalParams(s, null);
-            OctetStreamData output = (OctetStreamData) ts.transform(data, null);
-            Scanner scan = new Scanner(output.getOctetStream()).useDelimiter("\\A");
-            String result = scan.hasNext() ? scan.next() : "";
-            System.out.println("output: " + result);
+                OctetStreamData output = (OctetStreamData) ts.transform(data, null);
+                Scanner scan = new Scanner(output.getOctetStream()).useDelimiter("\\A");
+                String result = scan.hasNext() ? scan.next() : "";
+                printResult("output: \n" + result);
+            }
 
         } catch (NoSuchAlgorithmException | InvalidAlgorithmParameterException ex) {
             throw new AlgorithmInstantiationException(ex);
-        } catch (Exception ex) {
+        } catch (UnsupportedOperationException | InvalidParameterException | ProviderException | TransformException | ParserConfigurationException
+                | SAXException | IOException | MarshalException ex) {
             throw new AlgorithmRunException(ex);
         }
-
+    }
+    
+    /**
+     * Prevents from using aliases that cause failures during transformation
+     * 'cause TransformService is unable to find Canonicalizer for that
+     * particular alias
+     *
+     * @param alias
+     * @return whether the alias is a troublemaker
+     */
+    private static boolean isTroublemaker(String alias) {
+        return alias.equals("EXCLUSIVE") || alias.equals("INCLUSIVE_WITH_COMMENTS") || alias.equals("INCLUSIVE") || alias.equals("EXCLUSIVE_WITH_COMMENTS");
+    }
+    
+    /**
+     * This method checks if the algorithm is known to be working. If it is
+     * working, it doesn't mean it's working for all aliases. See
+     * isTroublemaking method.
+     *
+     * @param algorithm
+     * @return
+     */
+    private static boolean isKindOfWorkingAlgorithm(String algorithm) {
+        return algorithm.endsWith("/xml-c14n11#WithComments") || algorithm.endsWith("/xml-exc-c14n#")
+                || algorithm.endsWith("/REC-xml-c14n-20010315#WithComments") || algorithm.endsWith("/REC-xml-c14n-20010315")
+                || algorithm.endsWith("/xml-exc-c14n#WithComments") || algorithm.endsWith("/xml-c14n11");
     }
 
     @Override
