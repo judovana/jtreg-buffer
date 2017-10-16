cfe65cb98c60ad6a63b303d83884447f68d5d3a5
Jiri Vanek <jvanek@redhat.com>
2017-10-16 11:17:53 +0200

    Added reprodcuer for 1484079

---
+++ test/reproducers/1484079/TestJava1484079.java
@@ -0,0 +1,30 @@
+// see: https://stackoverflow.com/questions/45569367/upgrade-rhel-from-7-3-to-7-4-arrayindexoutofboundsexception-in-sun-font-composihttps://stackoverflow.com/questions/45569367/upgrade-rhel-from-7-3-to-7-4-arrayindexoutofboundsexception-in-sun-font-composi
+import java.awt.*;
+import java.awt.font.*;
+
+/*
+ * @test
+ * @bug 1484079
+ * @summary   - when only stixfonts are installed, there is fail
+ * @run main/timeout=600/othervm     TestJava1484079
+ */
+
+public class TestJava1484079 {
+  public static void main(String[] args) {
+
+    String []fontFamilies = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    for (String font : fontFamilies) {
+      System.err.println(font);
+    }
+
+    Font font = new Font("SansSerif", Font.PLAIN, 12);
+    //Font font = new Font("STIX", Font.PLAIN, 12);
+
+    FontRenderContext frc = new FontRenderContext(null, false, false);
+    TextLayout layout = new TextLayout("\ude00", font, frc);
+    layout.getCaretShapes(0);
+    System.out.println(layout);
+  }
+}
+
+
