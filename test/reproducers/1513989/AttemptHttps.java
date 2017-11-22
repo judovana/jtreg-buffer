626826a188b838d55688930d0b69179bb41305ef
Jiri Vanek <jvanek@redhat.com>
2017-11-22 09:44:02 +0100

    Added test for https simple connection

---
+++ test/reproducers/1513989/AttemptHttps.java
@@ -0,0 +1,19 @@
+
+ /*
+ * @test
+ * @bug 1513989
+ * @summary  verify that cacerts are set right and jdk can suceed in https connection
+ * @run main/timeout=1/othervm AttemptHttps
+ */
+
+
+import java.net.*;
+import java.io.*;
+
+public class AttemptHttps {
+    public static void main(String... args) throws IOException{
+        URL u = new URL("https://www.redhat.com/index.html");
+        URLConnection c = u.openConnection();
+        c.connect();
+    }
+}
