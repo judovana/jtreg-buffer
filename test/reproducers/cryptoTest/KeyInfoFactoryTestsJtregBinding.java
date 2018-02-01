443d60ca3a94fb01cac922a905250321c7d63364
Zdenek Zambersky <zzambers@redhat.com>
2018-02-01 14:50:38 +0100

    crypto tests build tag cleanup: formated, removed unnecessary classes

--- test/reproducers/cryptoTest/KeyInfoFactoryTestsJtregBinding.java
+++ test/reproducers/cryptoTest/KeyInfoFactoryTestsJtregBinding.java
@@ -1,7 +1,13 @@
 /*
  * @test
  * @bug 6666666
- * @build cryptotest.Settings cryptotest.utils.TestResult cryptotest.utils.Misc cryptotest.utils.AlgorithmRunException cryptotest.utils.KeysNaiveGenerator cryptotest.utils.Xml cryptotest.utils.AlgorithmInstantiationException cryptotest.utils.AlgorithmTest cryptotest.utils.ClassFinder cryptotest.CryptoTest cryptotest.tests.CertStoreTests cryptotest.tests.KeyAgreementTests cryptotest.tests.SaslServerFactoryTests cryptotest.tests.KeyPairGeneratorTests cryptotest.tests.KeyStoreTests cryptotest.tests.TestServices cryptotest.tests.KeyInfoFactoryTests cryptotest.tests.CertPathBuilderTests cryptotest.tests.AlgorithmParametersTests cryptotest.tests.AlgorithmParameterGeneratorTests cryptotest.tests.SSLContextTests cryptotest.tests.SecretKeyFactoryTests cryptotest.tests.CertPathValidatorTests cryptotest.tests.PolicyTests cryptotest.tests.SaslClientFactoryTests cryptotest.tests.KeyGeneratorTests cryptotest.tests.CertificateFactoryTests cryptotest.tests.ConfigurationTests cryptotest.tests.SignatureTests cryptotest.tests.KeyFactoryTests cryptotest.tests.TrustManagerFactoryTests cryptotest.tests.MessageDigestTests cryptotest.tests.TestProviders cryptotest.tests.XMLSignatureFactoryTest cryptotest.tests.CipherTests cryptotest.tests.MacTests cryptotest.tests.SecureRandomTests cryptotest.tests.GssApiMechanismTests cryptotest.tests.TerminalFactoryTests cryptotest.tests.TransformServiceTests cryptotest.tests.KeyManagerFactoryTests
+ * @build cryptotest.tests.KeyInfoFactoryTests
+ *        cryptotest.Settings
+ *        cryptotest.utils.AlgorithmInstantiationException
+ *        cryptotest.utils.AlgorithmRunException
+ *        cryptotest.utils.AlgorithmTest
+ *        cryptotest.utils.KeysNaiveGenerator
+ *        cryptotest.utils.TestResult
  * @run main/othervm cryptotest.tests.KeyInfoFactoryTests
  */
 
