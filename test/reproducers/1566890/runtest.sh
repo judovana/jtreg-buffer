a65b05303c13e7cdc873f749475d073266f8f535
Zdenek Zambersky <zzambers@redhat.com>
2018-05-16 20:10:05 +0200

    Added test for 1566890

---
+++ test/reproducers/1566890/runtest.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# @test
+# @bug 1566890
+# @summary test for 1566890
+# @run shell/timeout=100 runtest.sh
+
+set -eu
+
+FS="/"
+JAVA=${TESTJAVA}${FS}jre${FS}bin${FS}java
+
+strace -f "${JAVA}" -help &> strace.log
+
+if ! cat strace.log | grep "prctl(0x35 .*, 0, 0x4, 0, 0).*"  ; then
+	echo "Missing expected call in strace output"
+	exit 1
+fi
+
+exit 0
