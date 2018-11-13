347d4b3eb3ce322cf3b359363c8169becdb236fe
Zdenek Zambersky <zzambers@redhat.com>
2018-11-13 19:43:05 +0100

    1566890: fixed for rhel-8

--- test/reproducers/1566890/speculative-store-bypass.sh
+++ test/reproducers/1566890/speculative-store-bypass.sh
@@ -9,10 +9,16 @@ set -eu
 FS="/"
 JAVA=${TESTJAVA}${FS}jre${FS}bin${FS}java
 
+if ! type strace &> /dev/null ; then
+	echo "ERROR: Missing strace!" 1>&2
+	exit 1
+fi
+
 strace -f "${JAVA}" -help &> strace.log
 
-if ! cat strace.log | grep "prctl(0x35 .*, 0, 0x4, 0, 0).*"  ; then
-	echo "Missing expected call in strace output"
+if ! cat strace.log | grep "prctl(0x35 .*, 0, 0x4, 0, 0).*" \
+&& ! cat strace.log | grep "prctl(PR_SET_SPECULATION_CTRL, PR_SPEC_STORE_BYPASS, PR_SPEC_DISABLE.*" ; then
+	echo "FAILED: Missing expected call in strace output" 1>&2
 	exit 1
 fi
 
