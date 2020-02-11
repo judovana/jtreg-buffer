0bfd9c47ef295f4e253227a728f2db4f8b429ebb
Zdenek Zambersky <zzambers@redhat.com>
2020-02-11 17:58:33 +0100

    Added reproducer for 1707907 Stabilize Shenandoah Traversal mode

---
+++ test/reproducers/1707907/testStabilizeShenandoahTraversal.sh
@@ -0,0 +1,29 @@
+#
+# @test
+# @bug 1707907
+# @requires jdk.version.major >= 8
+# @summary Stabilize Shenandoah Traversal mode
+# @run shell testStabilizeShenandoahTraversal.sh
+#
+
+set -eu
+
+# set platform-dependent variables
+OS=`uname -s`
+case "$OS" in
+  SunOS | Linux )
+    FS="/"
+    ;;
+  Windows_* | CYGWIN_NT* )
+    FS="\\"
+    ;;
+  * )
+    echo "Unrecognized system!"
+    exit 1;
+    ;;
+esac
+
+
+
+JAVA=${TESTJAVA}${FS}bin${FS}java
+$JAVA -XX:+UseShenandoahGC -XX:ShenandoahGCMode=traversal -version
