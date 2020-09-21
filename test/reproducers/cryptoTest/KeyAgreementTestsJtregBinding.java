ea467291cbac6bc8f7e528fda89965bd3934276c
Zdenek Zambersky <zzambers@redhat.com>
2020-09-21 16:01:25 +0200

    Fixed KeyAgreementTestsJtregBinding.java and SecretKeyFactoryTestsJtregBinding.java

--- test/reproducers/cryptoTest/KeyAgreementTestsJtregBinding.java
+++ test/reproducers/cryptoTest/KeyAgreementTestsJtregBinding.java
@@ -1,6 +1,7 @@
 /*
  * @test
  * @modules java.base/com.sun.crypto.provider
+ *          java.base/sun.security.internal.spec
  *          jdk.crypto.ec/sun.security.ec
  * @bug 6666666
  * @build cryptotest.tests.KeyAgreementTests
@@ -8,6 +9,8 @@
  *        cryptotest.utils.AlgorithmInstantiationException
  *        cryptotest.utils.AlgorithmRunException
  *        cryptotest.utils.AlgorithmTest
+ *        cryptotest.utils.KeysNaiveGenerator
+ *        cryptotest.utils.Misc
  *        cryptotest.utils.TestResult
  * @run main/othervm cryptotest.tests.KeyAgreementTests
  */
--- test/reproducers/cryptoTest/SecretKeyFactoryTestsJtregBinding.java
+++ test/reproducers/cryptoTest/SecretKeyFactoryTestsJtregBinding.java
@@ -1,11 +1,14 @@
 /*
  * @test
  * @bug 6666666
+ * @modules java.base/sun.security.internal.spec
  * @build cryptotest.tests.SecretKeyFactoryTests
  *        cryptotest.Settings
  *        cryptotest.utils.AlgorithmInstantiationException
  *        cryptotest.utils.AlgorithmRunException
  *        cryptotest.utils.AlgorithmTest
+ *        cryptotest.utils.KeysNaiveGenerator
+ *        cryptotest.utils.Misc
  *        cryptotest.utils.TestResult
  * @run main/othervm cryptotest.tests.SecretKeyFactoryTests
  */
