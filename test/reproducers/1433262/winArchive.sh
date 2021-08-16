fab0d1759f61ca6ebe747fe818cc90b2676d95a0
Jiri Vanek <jvanek@redhat.com>
2021-08-16 18:39:34 +0200

    ignoring the  8192m.windows.zip
    It have nothing to do with original 8192m.zip issue (which is going to be fixed), but this is not going to be fixed any time soon. Its fate is to be discussed

--- test/reproducers/1433262/winArchive.sh
+++ test/reproducers/1433262/winArchive.sh
@@ -2,6 +2,7 @@
 ## @test winArchive.sh
 ## @bug 1433262
 ## @summary windows archive of input >4gb fails
+## @ignore the 8192m.windows.zip have nothing to do with original 8192m.zip issue (which is going to be fixed), but this is not going to be fixed
 
 set -ex
 
