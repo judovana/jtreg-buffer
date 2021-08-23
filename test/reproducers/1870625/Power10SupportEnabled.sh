866d06dcffa559a6deae8c9de8eaa2777a221ca8
jandrlik <jandrlik@redhat.com>
2021-08-23 13:20:39 +0200

    added reproducer for testing of power10 enablement on ojdk11 and 17 LTS packages

---
+++ test/reproducers/1870625/Power10SupportEnabled.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+##
+## @test Power10SupportEnabled.sh
+## @bug 1870625
+## @summary test whether power 10 support is enabled
+## @run shell Power10SupportEnabled.sh
+## @requires jdk.version.major >= 11
+##
+
+${TESTJAVA} -version -XX:+UseByteReverseInstructions
