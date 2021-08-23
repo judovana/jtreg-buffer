fb587007578a2ead7063c27e6289b04b8615cad3
Radek Cap <rcap@redhat.com>
2021-08-23 17:40:17 +0200

    Exclude Windows from Power 10 support test

--- test/reproducers/1870625/Power10SupportEnabled.sh
+++ test/reproducers/1870625/Power10SupportEnabled.sh
@@ -6,6 +6,7 @@
 ## @summary test whether power 10 support is enabled
 ## @run shell Power10SupportEnabled.sh
 ## @requires jdk.version.major >= 11
+## @requires os.family != "windows"
 ##
 
 ${TESTJAVA} -version -XX:+UseByteReverseInstructions
