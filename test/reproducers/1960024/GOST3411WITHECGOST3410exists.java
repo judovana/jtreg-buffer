5b2fc9aebc0455a7173089a3b80aeb924f66ad66
Radek Cap <rcap@redhat.com>
2022-01-03 14:04:08 +0100

    Split GOST3411WITHECGOST3410 to <17 and >=17

--- test/reproducers/1960024/GOST3411WITHECGOST3410exists.java
+++ test/reproducers/1960024/GOST3411WITHECGOST3410exists.java
@@ -1,25 +1,40 @@
-
 /*
    @test
    @library  bcprov-jdk15on-168.jar
    @bug 1960024
    @bug 8266279
+   @requires jdk.version.major >= 17
    @summary JDK-8266929: 8u292 Unable to use algorithms from 3p providers
    @author Maybe Severin
    @run main GOST3411WITHECGOST3410exists
 */
 
-
-
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 
 public class GOST3411WITHECGOST3410exists {
-    public static void main (String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
+
+        String algorithmName = "GOST3411WITHECGOST3410";
+        String algorithmProvider = "BC";
+
         Security.addProvider(new BouncyCastleProvider());
-        new javax.crypto.EncryptedPrivateKeyInfo("GOST3411WITHECGOST3410", new byte[]{0});
-        Security.removeProvider("BC");
-        new javax.crypto.EncryptedPrivateKeyInfo("GOST3411WITHECGOST3410", new byte[]{0}); // this second one is failing on u292
+        checkAlgoritmhPresence(algorithmName);
+
+        Security.removeProvider(algorithmProvider);
+        try {
+            checkAlgoritmhPresence(algorithmName);
+        } catch (NoSuchAlgorithmException e) {
+            // expected exception
+            return;
+        }
+
+        throw new RuntimeException("No SuchAlgorithmException not thrown");
+    }
+
+    private static void checkAlgoritmhPresence(String gost3411WITHECGOST3410) throws NoSuchAlgorithmException {
+        new javax.crypto.EncryptedPrivateKeyInfo(gost3411WITHECGOST3410, new byte[]{0});
     }
 }
-
---
+++ test/reproducers/1960024/GOST3411WITHECGOST3410exists16AndLower.java
@@ -0,0 +1,22 @@
+/*
+   @test
+   @library  bcprov-jdk15on-168.jar
+   @bug 1960024
+   @bug 8266279
+   @requires jdk.version.major < 17
+   @summary JDK-8266929: 8u292 Unable to use algorithms from 3p providers
+   @author Maybe Severin
+   @run main GOST3411WITHECGOST3410exists16AndLower
+*/
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.Security;
+
+public class GOST3411WITHECGOST3410exists16AndLower {
+    public static void main(String[] args) throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+        new javax.crypto.EncryptedPrivateKeyInfo("GOST3411WITHECGOST3410", new byte[]{0});
+        Security.removeProvider("BC");
+        new javax.crypto.EncryptedPrivateKeyInfo("GOST3411WITHECGOST3410", new byte[]{0}); // this second one is failing on u292
+    }
+}
