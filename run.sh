5b66375ff5f4341aff01710f91745acedc4904f3
Radek Cap <rcap@redhat.com>
2022-01-07 08:26:18 +0100

    Process OTOOL_ variables

--- run.sh
+++ run.sh
@@ -13,10 +13,17 @@ fi;
 
 echo Running with $JAVA...
 
+envVarArg="-e:CUSTOM_DUMMY_VARIABLE=true"
+keys=$(env | grep OTOOL_ | sed "s/=.*//")
+for key in $keys; do
+  envVarArg="$envVarArg,$key"
+done
+
 mkdir -p test.${TIME}/jdk/JTwork test.${TIME}/jdk/JTreport
 java -jar jtreg/lib/jtreg.jar -v1 -a -ignore:quiet \
 		-w:test.${TIME}/jdk/JTwork -r:test.${TIME}/jdk/JTreport \
 		-jdk:$JAVA \
 		$BUGID \
+		$envVarArg \
 		test \
 	    | tee test.${TIME}/tests.log
