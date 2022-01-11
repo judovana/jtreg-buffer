aab4fb360fca38d682dce887f7ad2362d0d43edb
Jiri Vanek <jvanek@redhat.com>
2022-01-11 19:13:00 +0100

    bumped test/reproducers/6913047/Bug6913047.class to workalso with jdk11 and up

--- test/reproducers/1897602/TestDiagnosticCommandImpl.sh
+++ test/reproducers/1897602/TestDiagnosticCommandImpl.sh
@@ -39,10 +39,14 @@ case "$OS" in
     ;;
 esac
 
+if [ "0$OTOOL_JDK_VERSION" -gt "8" ]; then
+  EXTFLAGS='--add-modules jdk.crypto.cryptoki,java.base --add-exports jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED --add-exports java.base/javax.crypto=ALL-UNNAMED --add-opens java.base/javax.crypto=ALL-UNNAMED'
+fi
+
 set -exo pipefail
 
-$TESTJAVA/bin/javac -d . $TESTSRC/TestDiagnosticCommandImpl.java
-$TESTJAVA/bin/java -Xcheck:jni TestDiagnosticCommandImpl 2>&1 | tee TestDiagnosticCommandImpl.out
+$TESTJAVA/bin/javac $EXTFLAGS -d . $TESTSRC/TestDiagnosticCommandImpl.java
+$TESTJAVA/bin/java $EXTFLAGS -Xcheck:jni TestDiagnosticCommandImpl 2>&1 | tee TestDiagnosticCommandImpl.out
 
 if cat TestDiagnosticCommandImpl.out | grep -i -e WARNING ; then
   echo failed
