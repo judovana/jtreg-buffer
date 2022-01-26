3dd49503fbb8646afc33e9937e25fd6d4d66fc26
Zdenek Zambersky <zzambers@redhat.com>
2022-01-26 21:44:49 +0100

    1463098/cgroup-memory-limit-respected-systemd.sh: removed cases with old UseCGroupMemoryLimitForHeap option

--- test/reproducers/1463098/cgroup-memory-limit-respected-systemd.sh
+++ test/reproducers/1463098/cgroup-memory-limit-respected-systemd.sh
@@ -76,13 +76,9 @@ EOF
 sudo -n systemctl daemon-reload
 sudo -n systemctl restart "${ojdkSliceName}"
 # cgroup
-sudo -n systemd-run --slice "${ojdkSliceName}" --scope java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+PrintFlagsFinal -version 2>&1 | tee java-cgroup-options.log || :
 sudo -n systemd-run --slice "${ojdkSliceName}" --scope java -XX:+PrintFlagsFinal -version 2>&1 | tee java-cgroup-no-options.log || :
 # non-cgroup
-sudo -n java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+PrintFlagsFinal -version 2>&1 | tee java-direct-options.log || :
 sudo -n java -XX:+PrintFlagsFinal -version 2>&1 | tee java-direct-no-options.log || :
 
-checkMaxHeapSize java-cgroup-options.log 1 || exit 1
 checkMaxHeapSize java-cgroup-no-options.log 1 || exit 1
-checkMaxHeapSize java-direct-options.log 0 || exit 1
 checkMaxHeapSize java-direct-no-options.log 0 || exit 1
