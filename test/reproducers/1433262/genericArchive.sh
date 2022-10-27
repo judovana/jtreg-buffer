bc5f8f702a02373ae4f0a8dbc5ffdf9752290bf5
Zdenek Zambersky <zzambers@redhat.com>
2022-10-28 00:33:54 +0200

    1433262/genericArchive.sh: skipped for builds, where it is expected to fail

--- test/reproducers/1433262/genericArchive.sh
+++ test/reproducers/1433262/genericArchive.sh
@@ -5,6 +5,20 @@
 
 set -ex
 
+if [ 8 = "${OTOOL_JDK_VERSION:-}" ] ; then
+    if [ "el" = "${OTOOL_BUILD_OS_NAME:-}" ] && ! [ "7" = "${OTOOL_BUILD_OS_VERSION:-}" ] ; then
+        # Only affects JDK8, fix is only in rhel-7 builds (rpms + portables)
+        # in form of rpm patch. See:
+        # https://bugzilla.redhat.com/show_bug.cgi?id=1433262
+        # Failure on other OSes is known, reported (multiple times). Plan is
+        # to upstream the fix (backport) rather than bring it to other
+        # OSes (check e-mails). Skipping builds, where it is expected to fail.
+        # Can be reenabled once patch is upstramed.
+        echo "Fix for this is only in rhel-7 builds, skipping..."
+        exit 0
+    fi
+fi
+
 FS="/"
 JAVAC=${TESTJAVA}${FS}bin${FS}javac
 JAVA=${TESTJAVA}${FS}bin${FS}java
