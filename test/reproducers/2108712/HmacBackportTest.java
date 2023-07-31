d400bf1c71fd0d80d3f274a0c30cc2e439e1f4f9
Zdenek Zambersky <zzambers@redhat.com>
2023-07-31 16:06:14 +0200

    2108712/HmacBackportTest.java: Exclude on fedora

--- test/reproducers/2108712/HmacBackportTest.java
+++ test/reproducers/2108712/HmacBackportTest.java
@@ -10,7 +10,22 @@
 import javax.crypto.KeyGenerator;
 
 public class HmacBackportTest {
+
+        private static boolean isFedoraBuild() {
+            String bos = null;
+            try {
+                bos = System.getenv("OTOOL_BUILD_OS_NAME");
+            } catch (Exception e) { /* ignore */ }
+            return "f".equals(bos);
+        }
+
         public static void main(String[] args) throws Exception {
+                if (isFedoraBuild()) {
+                    // backport of JDK-8242332 is only in Rhel builds
+                    // https://bugzilla.redhat.com/show_bug.cgi?id=2108712
+                    System.out.println("Does not apply to fedora builds, skipping...");
+                    return;
+                }
                 KeyGenerator kg = KeyGenerator.getInstance("HmacSHA1");
                 System.out.println(kg);
                 System.out.println("KeyGenerator provider: " + kg.getProvider().getName());
