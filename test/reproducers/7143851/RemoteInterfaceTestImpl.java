7fb9b8060397a77ad8193974e3741a6d18d2592f
Jiri Vanek <jvanek@redhat.com>
2023-11-01 18:17:03 +0100

    Removed tests duplicated in security reproducers

--- test/reproducers/7143851/RemoteInterfaceTestImpl.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
- 
-public class RemoteInterfaceTestImpl implements RemoteInterfaceTest {
-    public void test() throws RemoteException {
-        // test code; ignore
-    }
-}
