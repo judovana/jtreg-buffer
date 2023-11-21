d997c2aab1555ec06d24acc4d7572aef1cc59dc8
Jiri Vanek <jvanek@redhat.com>
2023-11-21 12:56:21 +0100

    grepped out  Warning: SIGPIPE handler expected
    todo, consult
    fixed also modules handling

--- test/reproducers/1897602/TestDiagnosticCommandImpl.sh
+++ test/reproducers/1897602/TestDiagnosticCommandImpl.sh
@@ -4,7 +4,6 @@
 # @test
 # @bug 1897602
 # @summary Warnings when using ManagementFactory.getPlatformMBeanServer with -Xcheck:jni VM argument
-# @requires jdk.version.major > 8
 # @run shell TestDiagnosticCommandImpl.sh
 
 if [ "x${TESTSRC}" = "x" ] ; then
@@ -39,9 +38,12 @@ case "$OS" in
     ;;
 esac
 
-if [ "0$OTOOL_JDK_VERSION" -gt "8" ]; then
+if $TESTJAVA/bin/java -version 2>&1 | grep 1.8.0 ; then
+  EXTFLAGS=""
+else
   EXTFLAGS='--add-modules jdk.crypto.cryptoki,java.base --add-exports jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED --add-opens jdk.crypto.cryptoki/sun.security.pkcs11=ALL-UNNAMED --add-exports java.base/javax.crypto=ALL-UNNAMED --add-opens java.base/javax.crypto=ALL-UNNAMED'
 fi
+
 
 set -exo pipefail
 
@@ -49,8 +51,7 @@ $TESTJAVA/bin/javac $EXTFLAGS -d . $TESTSRC/TestDiagnosticCommandImpl.java
 $TESTJAVA/bin/java $EXTFLAGS -Xcheck:jni TestDiagnosticCommandImpl 2>&1 | tee TestDiagnosticCommandImpl.out
 
 sed -i 's;.*warning.*jfr.*LeakProfiler .*;jfr issue removed, see runner;g' TestDiagnosticCommandImpl.out
-
-
+sed -i 's;.*Warning: SIGPIPE handler expected;SIGPIPE issue removed, see runner;g' TestDiagnosticCommandImpl.out
 
 if cat TestDiagnosticCommandImpl.out | grep -i -e WARNING ; then
   echo failed
