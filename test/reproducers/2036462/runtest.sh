9df69dff6b6b9ef52e11a9200f6ec486db2cee17
Jiri Vanek <jvanek@redhat.com>
2023-11-21 11:53:03 +0100

    2036462/runtest.sh now correclty set modules

--- test/reproducers/2036462/runtest.sh
+++ test/reproducers/2036462/runtest.sh
@@ -5,18 +5,19 @@ set -x
 
 FS="/"
 JAVAC=${TESTJAVA}${FS}bin${FS}javac
+JAVA=${TESTJAVA}${FS}bin${FS}java
 
 if [ "x$TESTSRC" == x ] ; then
   TESTSRC=.
 fi
 
-# this is needed for ojdk 16 and above
-FLAGS="--add-modules jdk.crypto.cryptoki --add-exports jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED"
-
-# removing the above for ojdk lower than 16
-if [ 9 -gt $OTOOL_JDK_VERSION ]; then
-    FLAGS=""
+if $JAVA -version 2>&1 | grep 1.8.0 ; then
+  FLAGS=""
+else
+  # this is needed for ojdk 16 and above
+  FLAGS="--add-modules jdk.crypto.cryptoki --add-exports jdk.crypto.cryptoki/sun.security.pkcs11.wrapper=ALL-UNNAMED"
 fi
+
 
 # only testing whether the Test.java is buildable, dont need to run it (see bug)
 ${JAVAC} -d . $FLAGS ${TESTSRC}/Test.java
