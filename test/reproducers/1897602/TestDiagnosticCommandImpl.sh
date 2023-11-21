28d246dc32e5702dd70d936bc774b6fbd5dbda98
Jiri Vanek <jvanek@redhat.com>
2023-11-21 16:59:16 +0100

    adjsuted 1897602 to pass on all jdks, and been skipped on 8

--- test/reproducers/1897602/TestDiagnosticCommandImpl.sh
+++ test/reproducers/1897602/TestDiagnosticCommandImpl.sh
@@ -4,7 +4,23 @@
 # @test
 # @bug 1897602
 # @summary Warnings when using ManagementFactory.getPlatformMBeanServer with -Xcheck:jni VM argument
+# @requires jdk.version.major > 8
 # @run shell TestDiagnosticCommandImpl.sh
+
+# Note, that on jdk8, this fails with differen trace then the original issue was hiting:
+# https://bugzilla.redhat.com/show_bug.cgi?id=1897602
+# > As for https://bugs.openjdk.java.net/browse/JDK-8260436, it s already in
+# > 11u. Any plans with 8u?
+# No, it's not an issue there.
+# Different bug.
+# $ /usr/lib/jvm/java-1.8.0-openjdk/bin/java -Xcheck:jni TestDiagnosticCommandImpl 
+# WARNING in native method: JNI call made without checking exceptions when required to from CallStaticObjectMethod
+# ...
+# Fixed in later JDKs with:
+# https://hg.openjdk.java.net/jdk-updates/jdk11u-dev/rev/a16236cd61d7
+# ...
+# thus requires jdk.version.major > 8...
+
 
 if [ "x${TESTSRC}" = "x" ] ; then
   TESTSRC=.
@@ -51,7 +67,7 @@ $TESTJAVA/bin/javac $EXTFLAGS -d . $TESTSRC/TestDiagnosticCommandImpl.java
 $TESTJAVA/bin/java $EXTFLAGS -Xcheck:jni TestDiagnosticCommandImpl 2>&1 | tee TestDiagnosticCommandImpl.out
 
 sed -i 's;.*warning.*jfr.*LeakProfiler .*;jfr issue removed, see runner;g' TestDiagnosticCommandImpl.out
-sed -i 's;.*Warning: SIGPIPE handler expected;SIGPIPE issue removed, see runner;g' TestDiagnosticCommandImpl.out
+sed -i 's;.*Warning: SIGPIPE handler.*;SIGPIPE issue removed, see runner;g' TestDiagnosticCommandImpl.out
 
 if cat TestDiagnosticCommandImpl.out | grep -i -e WARNING ; then
   echo failed
