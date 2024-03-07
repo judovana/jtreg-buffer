81aa1e8e3f551ebdd7e1ff272ea6f05cc6e3e172
Jiri Vanek <jvanek@redhat.com>
2024-03-07 16:55:57 +0100

    Two more tests excluded in msys with temurin jdk

--- test/reproducers/1870625/Power10SupportEnabled.sh
+++ test/reproducers/1870625/Power10SupportEnabled.sh
@@ -7,7 +7,7 @@ set -x
 ## @bug 1870625
 ## @summary test whether power 10 support is enabled
 ## @run shell Power10SupportEnabled.sh
-## @requires jdk.version.major >= 11
+## @requires jdk.version.major >= 11 & var.msys2.enabled == "false"
 ##
 
 ${TESTJAVA}/bin/java -version -XX:+UseByteReverseInstructions
